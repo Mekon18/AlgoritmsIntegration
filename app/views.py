ee49b058e2f400e4cebe3ecddaeca97e80b02d19
vladimirantonov <vova.antonov.99@bk.ru>
2019-07-11 12:52:01 +0300

    no message

--- app/views.py
+++ app/views.py
@@ -192,21 +192,31 @@ def fireflies(request):
     #form = ParamForm(request.POST or None)
     return render(request, 'Fireflies/homePage.html', locals())
 
+nturn = 0
+num_worms = 0
+influence_factor = 0
+max_jitter = 0
+start = 0
+end =0 
+function = ''
+
+
 def wait(request):
-    #if request.method == "POST": 
-        #global nturns, num_worms, influence_factor, max_jitter, start, end, function
+    if request.method == "POST": 
+        global nturns, num_worms, influence_factor, max_jitter, start, end, function
+        nturns = request.POST.get('nturns', '')
+        num_worms = request.POST.get('num_worms', '')
+        influence_factor = request.POST.get('influence_factor', '')
+        max_jitter = request.POST.get('max_jitter', '')
+        start = request.POST.get('start', '')
+        end = request.POST.get('end', '')
+        function = request.POST.get('function', '')
     return render(request, 'Fireflies/wait.html')
 
 def result(request):
     #form = ParamForm(request.POST)
     #if request.method == "POST": #and form.is_valid():
-    nturns = request.POST.get('nturns', '')
-    num_worms = request.POST.get('num_worms', '')
-    influence_factor = request.POST.get('influence_factor', '')
-    max_jitter = request.POST.get('max_jitter', '')
-    start = request.POST.get('start', '')
-    end = request.POST.get('end', '')
-    function = request.POST.get('function', '')
+    
     results = go(int(nturns), int(num_worms), int(influence_factor), float(max_jitter), float(start), float(end), function)
 
     return render(request, 'Fireflies/basic.html', {'values' : ['Функция ' + function], 'result' : results})
